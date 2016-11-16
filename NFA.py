32d807510f4774074e4c5ff7d4352fd533479bb1
alessio <cecconi.1404997@studenti.uniroma1.it>
2016-11-16 11:24:34 +0100

    Moved  import/export and rendering functions in automata_IO, making automata modules independent from IO

--- NFA.py
+++ NFA.py
@@ -1,7 +1,5 @@
-import json
 from copy import deepcopy
 from itertools import product as cartesian_product
-import pydot
 import DFA
 
 
@@ -28,74 +26,6 @@ import DFA
 # transitions = {}  # key (state in states, action in alphabet) value [set of arriving stateS in states]
 #
 # nfa = [alphabet, states, initial_state, final, transition]
-
-# Export a nfa "object" to a json file
-# TODO nfa_to_json
-def nfa_to_json(nfa):
-    return
-
-
-# Export a dfa "object" to a DOT file
-# TODO dfa_to_dot
-def nfa_to_dot(dfa):
-    return
-
-
-# Import a nfa from a json file
-def nfa_json_importer(input_file):
-    file = open(input_file)
-    json_file = json.load(file)
-    # TODO exception handling while JSON deconding/IO error
-    alphabet = set(json_file['alphabet'])
-    states = set(json_file['states'])
-    initial_states = set(json_file['initial_states'])
-    accepting_states = set(json_file['accepting_states'])
-    transitions = {}  # key [state in states, action in alphabet] value [et of arriving states in states]
-    for p in json_file['transitions']:
-        transitions.setdefault((p[0], p[1]), set()).add(p[2])
-
-    # return list
-    # return [alphabet, states, initial_states, accepting_states, transitions]
-
-    # return map
-    nfa = {}
-    nfa['alphabet'] = alphabet
-    nfa['states'] = states
-    nfa['initial_states'] = initial_states
-    nfa['accepting_states'] = accepting_states
-    nfa['transitions'] = transitions
-    return nfa
-
-
-# Import a nfa from a dot file
-# TODO
-def nfa_dot_importer(input_file):
-    nfa = {}
-    return nfa
-
-
-def nfa_render(nfa, name):
-    g = pydot.Dot(graph_type='digraph')
-
-    fake = pydot.Node('fake', style='invisible')
-    g.add_node(fake)
-    for state in nfa['states']:
-        node = pydot.Node(state)
-        if state in nfa['initial_states']:
-            node.set_root(True)
-            g.add_edge(pydot.Edge(fake, node, style='bold'))
-
-        if state in nfa['accepting_states']:
-            node.set_shape('doublecircle')
-        g.add_node(node)
-
-    for transition in nfa['transitions']:
-        for dest in nfa['transitions'][transition]:
-            g.add_edge(pydot.Edge(transition[0], dest, label=transition[1]))
-
-    g.write_svg('img/' + name + '.svg')
-    g.write_dot('img/' + name + '.dot')
-    return
 
 
 # - NFAs intersection
