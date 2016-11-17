e5b71d9b7885fe6eb7e08da04fc5fffc4dcd9583
alessio <cecconi.1404997@studenti.uniroma1.it>
2016-11-17 15:00:29 +0100

    AFW unit test first implementation: just a stub

---
+++ tests/test_AFW.py
@@ -0,0 +1,63 @@
+from unittest import TestCase
+import unittest
+import AFW
+
+
+class TestRecursiveAcceptance(TestCase):
+    @unittest.skip("TestRecursiveAcceptance TODO")
+    def test___recursive_acceptance(self):
+        self.fail()
+
+
+class TestWordAcceptance(TestCase):
+    @unittest.skip("TestWordAcceptance TODO")
+    def test_word_acceptance(self):
+        self.fail()
+
+
+class TestNfaToAfwConversion(TestCase):
+    @unittest.skip("TestNfaToAfwConversion TODO")
+    def test_nfa_to_afw_conversion(self):
+        self.fail()
+
+
+class TestAfwToNfaConversion(TestCase):
+    @unittest.skip("TestAfwToNfaConversion TODO")
+    def test_afw_to_nfa_conversion(self):
+        self.fail()
+
+
+class TestReplaceAll(TestCase):
+    @unittest.skip("TestReplace_all TODO")
+    def test___replace_all(self):
+        self.fail()
+
+
+class TestAfwComplementation(TestCase):
+    @unittest.skip("TestAfwComplementation TODO")
+    def test_afw_complementation(self):
+        self.fail()
+
+
+class TestAfwUnion(TestCase):
+    @unittest.skip("TestAfwUnion TODO")
+    def test_afw_union(self):
+        self.fail()
+
+
+class TestAfwIntersection(TestCase):
+    @unittest.skip(" TODO")
+    def test_afw_intersection(self):
+        self.fail()
+
+
+class TestAfwNonemptinessCheck(TestCase):
+    @unittest.skip("TestAfwNonemptinessCheck TODO")
+    def test_afw_nonemptiness_check(self):
+        self.fail()
+
+
+class TestAfwNonuniversalityCheck(TestCase):
+    @unittest.skip("TestAfwNonuniversalityCheck TODO")
+    def test_afw_nonuniversality_check(self):
+        self.fail()
