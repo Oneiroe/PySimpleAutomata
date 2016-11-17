c18f9c4591eb6bd31695cbbe5f8039b657991ef4
alessio <cecconi.1404997@studenti.uniroma1.it>
2016-11-17 12:36:50 +0100

    NFA unit test first implementation: just a stub

---
+++ tests/test_NFA.py
@@ -0,0 +1,57 @@
+from unittest import TestCase
+import unittest
+import NFA
+
+
+class TestNfaIntersection(TestCase):
+    @unittest.skip("TestNfaIntersection TODO")
+    def test_nfa_intersection(self):
+        self.fail()
+
+
+class TestNfaUnion(TestCase):
+    @unittest.skip("TestNfaUnion TODO")
+    def test_nfa_union(self):
+        self.fail()
+
+
+class TestNfaDeterminization(TestCase):
+    @unittest.skip("TestNfaDeterminization TODO")
+    def test_nfa_determinization(self):
+        self.fail()
+
+
+class TestNfaComplementation(TestCase):
+    @unittest.skip("TestNfaComplementation TODO")
+    def test_nfa_complementation(self):
+        self.fail()
+
+
+class TestNfaNonemptinessCheck(TestCase):
+    @unittest.skip("TestNfaNonemptinessCheck TODO")
+    def test_nfa_nonemptiness_check(self):
+        self.fail()
+
+
+class TestNfaNonuniversalityCheck(TestCase):
+    @unittest.skip("TestNfaNonuniversalityCheck TODO")
+    def test_nfa_nonuniversality_check(self):
+        self.fail()
+
+
+class TestNfaInterestingnessCheck(TestCase):
+    @unittest.skip("TestNfaInterestingnessCheck TODO")
+    def test_nfa_interestingness_check(self):
+        self.fail()
+
+
+class TestRunAcceptance(TestCase):
+    @unittest.skip("TestRunAcceptance TODO")
+    def test_run_acceptance(self):
+        self.fail()
+
+
+class TestWordAcceptance(TestCase):
+    @unittest.skip("TestWordAcceptance TODO")
+    def test_word_acceptance(self):
+        self.fail()
