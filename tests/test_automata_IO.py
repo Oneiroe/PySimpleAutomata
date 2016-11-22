0278a50a05643fb4d3a9ee7d4727ae8b4c613c20
alessio <cecconi.1404997@studenti.uniroma1.it>
2016-11-22 12:53:51 +0100

    automata_IO initialized test suite

---
+++ tests/test_automata_IO.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+import unittest
+import DFA
+import NFA
+import AFW
+import automata_IO
+
+
+class TestPydotDfaRender(TestCase):
+    @unittest.skip
+    def test_pydot_dfa_render(self):
+        self.fail()
+
+
+class TestDfaRender(TestCase):
+    @unittest.skip
+    def test_dfa_render(self):
+        self.fail()
+
+
+class TestDfaJsonImporter(TestCase):
+    @unittest.skip
+    def test_dfa_json_importer(self):
+        self.fail()
+
+
+class TestDfaDotImporter(TestCase):
+    @unittest.skip
+    def test_dfa_dot_importer(self):
+        self.fail()
+
+
+class TestDfaToJson(TestCase):
+    @unittest.skip
+    def test_dfa_to_json(self):
+        self.fail()
+
+
+class TestDfaToDot(TestCase):
+    @unittest.skip
+    def test_dfa_to_dot(self):
+        self.fail()
