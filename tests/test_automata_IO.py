2f4680fa8f59505f44f6a4e384de38f8c4bb628e
alessio <cecconi.1404997@studenti.uniroma1.it>
2016-11-23 08:35:52 +0100

    automata_IO refactoring on dfaDotImporter

--- tests/test_automata_IO.py
+++ tests/test_automata_IO.py
@@ -48,15 +48,7 @@ class TestDfaJsonImporter(TestCase):
 class TestDfaDotImporter(TestCase):
     def setUp(self):
         self.maxDiff = None
-        # self.dfa_01 = automata_IO.dfa_dot_importer('./dot/dfa_intersection_1_test_01.dot')
-        # self.dfa_02 = automata_IO.dfa_dot_importer('./dot/dfa_intersection_2_test_01.dot')
-        # self.dfa_imported_intersect = automata_IO.dfa_dot_importer('./dot/dfa_intersection_test_01_intersection.dot')
-        # self.dfa_intersected = DFA.dfa_intersection(self.dfa_01, self.dfa_02)
-
-    def test_dfa_dot_importer(self):
-        """ Tests importing a dfa from a simple dot file"""
-        dfa_01 = automata_IO.dfa_dot_importer('./dot/dfa_intersection_1_test_01.dot')
-        dfa_test = {
+        self.dfa_test = {
             'alphabet': {'5c', '10c', 'gum'},
             'states': {'s0', 's1', 's2', 's3'},
             'initial_state': 's0',
@@ -69,12 +61,7 @@ class TestDfaDotImporter(TestCase):
                             ('s2', '10c'): 's3',
                             ('s3', 'gum'): 's0'}
         }
-        self.assertDictEqual(dfa_01, dfa_test)
-
-    def test_intersection_dfa_dot_importer(self):
-        """ Tests importing a dfa from a dot file derived from an intersection """
-        dfa_01 = automata_IO.dfa_dot_importer('./img/graphviz_dfa_intersection_render_test.dot')
-        dfa_test = {
+        self.dfa_test_02 = {
             'alphabet': {'5c', '10c', 'gum'},
             'states': {
                 ('s3', 't2'),
@@ -122,7 +109,16 @@ class TestDfaDotImporter(TestCase):
                 (('s0', 't0'), '5c'): ('s1', 't1')
             }
         }
-        self.assertDictEqual(dfa_01, dfa_test)
+
+    def test_dfa_dot_importer(self):
+        """ Tests importing a dfa from a simple dot file"""
+        dfa_01 = automata_IO.dfa_dot_importer('./dot/dfa_intersection_1_test_01.dot')
+        self.assertDictEqual(dfa_01, self.dfa_test)
+
+    def test_intersection_dfa_dot_importer(self):
+        """ Tests importing a dfa from a dot file derived from an intersection """
+        dfa_02 = automata_IO.dfa_dot_importer('./img/graphviz_dfa_intersection_render_test.dot')
+        self.assertDictEqual(dfa_02, self.dfa_test_02)
 
 
 class TestDfaToJson(TestCase):
